d334189b0a404c953ce51d8357f1946f6505e6a1
SuperGibbon <super_gibbon@hotmail.com>
2021-05-11 17:03:45 +0000

    Add delete functionality via button on recipe collapsible

--- app.py
+++ app.py
@@ -39,6 +39,13 @@ def add_recipes():
     return render_template("addrecipe.html", types=types)
 
 
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    flash("Recipe deleted")
+    return redirect(url_for("get_recipes"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
